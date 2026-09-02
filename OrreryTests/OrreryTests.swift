f4d7056c666b39861f7d8821ba081079d854ee5b
rishi-singh26 <rishisingh0831@gmail.com>
2026-09-02 09:39:27 +0530

    Initial Commit

---
+++ OrreryTests/OrreryTests.swift
@@ -0,0 +1,18 @@
+//
+//  OrreryTests.swift
+//  OrreryTests
+//
+//  Created by Rishi Singh on 02/09/26.
+//
+
+import Testing
+
+struct OrreryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
